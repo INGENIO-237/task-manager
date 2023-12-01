aae38077cfc6474460ed3036ddbf1f3c70f9d6b2
INGENIO <ingeniokhalif@gmail.com>
2023-12-01 09:34:58 +0100

    get task by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string) {
+    return this.tasks.find((task) => task.id === id);
+  }
+
   createTask(input: createTaskDto): Task {
     const { title } = input;
 
