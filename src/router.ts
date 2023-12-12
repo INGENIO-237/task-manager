f81448ddc10eec65fd528decc0e24233264d8c0f
INGENIO <ingeniokhalif@gmail.com>
2023-12-12 17:53:10 +0100

    created user schema & model

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,12 @@
 import { Express, Request, Response } from "express";
+import UsersRouter from "./routes/users.routes";
 
 const router = (server: Express) => {
   server.get("/healthcheck", (req: Request, res: Response) =>
     res.sendStatus(200)
   );
+
+  server.use("/users", UsersRouter);
 };
 
 export default router;
